d8e2090c584e9fc57e6e04f1f4bdacab73fd2c8f
Daniel Eche <danieleche7@gmail.com>
2020-01-14 08:04:47 +0100

    added by NielEche

--- index.php
+++ index.php
@@ -142,13 +142,14 @@
 	
 		<main id="content" class="content">
 			
-			<section  style="background-color: #0000ff;" class="vc_row bg-cover bg-center fullheight d-flex align-items-center  " data-row-bg="#0000ff">
+			<section  style="background-color: #0000ff;" class="vc_row bg-cover bg-center fullheight d-flex align-items-center">
 
 				<span style="background-color: #0000ff;" class="row-bg-loader"></span>
 
 				<div class="container-fluid">
 					<div class="row d-flex flex-wrap align-items-center">
-						<form action="#">
+						
+						<form action="#" style="width: 100%;">
 							<div id="question1" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 questions">
 								<div class="lqd-column text-center text-md-center" data-custom-animations="true" data-ca-options='{"triggerHandler":"inview","animationTarget":"all-childs","duration":"1200","startDelay":"1500","delay":100,"easing":"easeOutQuint","direction":"forward","initValues":{"scaleX":0.25,"scaleY":0.25,"opacity":0},"animations":{"scaleX":1,"scaleY":1,"opacity":1}}'>
 										
@@ -162,29 +163,71 @@
 							</div>
 						</form>
 
-						<form action="#">
-							<div id="question2" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 questions">
+						
+						<form id="regForm" action="#">
+							<div id="question2"  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 questions">
 								
-								<div class="lqd-column text-center text-md-center" data-custom-animations="true" data-ca-options='{"triggerHandler":"inview","animationTarget":"all-childs","duration":"1200","startDelay":"1500","delay":100,"easing":"easeOutQuint","direction":"forward","initValues":{"scaleX":0.25,"scaleY":0.25,"opacity":0},"animations":{"scaleX":1,"scaleY":1,"opacity":1}}'>
-									<img src="assets/img/icon/voiceslogo1.png">
+								<div  style="margin-top: 100px !important;" class="tab lqd-column text-center text-md-center" data-custom-animations="true" data-ca-options='{"triggerHandler":"inview","animationTarget":"all-childs","duration":"1200","startDelay":"1500","delay":100,"easing":"easeOutQuint","direction":"forward","initValues":{"scaleX":0.25,"scaleY":0.25,"opacity":0},"animations":{"scaleX":1,"scaleY":1,"opacity":1}}'>
+									<img src="assets/img/icon/camera.png">
+									<div style="margin-top: 20px !important;" class="">
+										<img src="assets/img/icon/selectfile.png">
+									</div>
+									
 								</div>
+								<div class="tab lqd-column text-center text-md-center" data-custom-animations="true" data-ca-options='{"triggerHandler":"inview","animationTarget":"all-childs","duration":"1200","startDelay":"1500","delay":100,"easing":"easeOutQuint","direction":"forward","initValues":{"scaleX":0.25,"scaleY":0.25,"opacity":0},"animations":{"scaleX":1,"scaleY":1,"opacity":1}}'>
+									<img src="assets/img/icon/voiceslogo1.png">
+									 <p><input placeholder="E-mail..." oninput="this.className = ''"></p>
+							  		<p><input placeholder="Phone..." oninput="this.className = ''"></p>
+								</div>
+								<div class="tab lqd-column text-center text-md-center" data-custom-animations="true" data-ca-options='{"triggerHandler":"inview","animationTarget":"all-childs","duration":"1200","startDelay":"1500","delay":100,"easing":"easeOutQuint","direction":"forward","initValues":{"scaleX":0.25,"scaleY":0.25,"opacity":0},"animations":{"scaleX":1,"scaleY":1,"opacity":1}}'>
+									<img src="assets/img/icon/voiceslogo1.png">
+									  <p><input placeholder="dd" oninput="this.className = ''"></p>
+									  <p><input placeholder="mm" oninput="this.className = ''"></p>
+									  <p><input placeholder="yyyy" oninput="this.className = ''"></p>
+								</div>
+								<div class="tab lqd-column text-center text-md-center" data-custom-animations="true" data-ca-options='{"triggerHandler":"inview","animationTarget":"all-childs","duration":"1200","startDelay":"1500","delay":100,"easing":"easeOutQuint","direction":"forward","initValues":{"scaleX":0.25,"scaleY":0.25,"opacity":0},"animations":{"scaleX":1,"scaleY":1,"opacity":1}}'>
+									<img src="assets/img/icon/voiceslogo1.png">
+									  <p><input placeholder="dd" oninput="this.className = ''"></p>
+									  <p><input placeholder="mm" oninput="this.className = ''"></p>
+									  <p><input placeholder="yyyy" oninput="this.className = ''"></p>
+								</div>
+								<div class="tab lqd-column text-center text-md-center" data-custom-animations="true" data-ca-options='{"triggerHandler":"inview","animationTarget":"all-childs","duration":"1200","startDelay":"1500","delay":100,"easing":"easeOutQuint","direction":"forward","initValues":{"scaleX":0.25,"scaleY":0.25,"opacity":0},"animations":{"scaleX":1,"scaleY":1,"opacity":1}}'>
+									<img src="assets/img/icon/voiceslogo1.png">
+									  <p><input placeholder="Username..." oninput="this.className = ''"></p>
+							  		  <p><input placeholder="Password..." oninput="this.className = ''"></p>
+								</div>
+								<div style="overflow:auto;">
+								  <div style="float:right;">
+								  	<div class="container-fluid nopadding">
+								  		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 nopadding">
+								  			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+								  				<button type="button" class="prevbutton" id="prevBtn" onclick="nextPrev(-1)">Previous</button>
+								  			</div>
+								  			<div></div>
+								  			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 nopadding">
+								  				 <button type="button" class="nopadding nextbutton" onclick="nextPrev(1)"><img src="assets/img/icon/next.png"></button>
+								  			</div>
+								  		</div>
+								  	</div>
+								  </div>
+								</div>
+								<!-- Circles which indicates the steps of the form: -->
+								<div style="text-align:center;margin-top:40px;">
+								  <span class="step"></span>
+								  <span class="step"></span>
+								  <span class="step"></span>
+								  <span class="step"></span>
+								</div>
+
 							</div>
 						</form>
+
+						
 					</div><!-- /.row -->
 				</div><!-- /.container -->
 
 			</section>
 
-			<section>
-				<form style="display: none !important;" id="form1">
-					<b>First Name:</b> <input type="text" name="firstName">
-					  <br><br>
-					  <b>Last Name: </b><input type="text" name="lastName">
-					  <br><br>
-					  <b> Age:</b>
-					  <br>
-				</form>
-			</section>
 
 	
 		</main><!-- /#content.content -->
@@ -242,6 +285,80 @@
 			}
 		});
 	});
+</script>
+<script>
+	var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form ...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  // ... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  // ... and run a function that displays the correct step indicator:
+  fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form... :
+  if (currentTab >= x.length) {
+    //...the form gets submitted:
+    document.getElementById("regForm").submit();
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+}
+
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x, y, i, valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == "") {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false:
+      valid = false;
+    }
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; // return the valid status
+}
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class to the current step:
+  x[n].className += " active";
+}
 </script>
 
 </body>
